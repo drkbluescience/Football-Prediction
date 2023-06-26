0bcfe0e3374569188750f152bfed38a8079caeac
enyse <zngnenise@gmail.com>
2023-06-26 17:18:16 +0300

    Add gitignore

---
+++ helper.py
@@ -0,0 +1,13 @@
+vals = {
+    'filename_new': 'files/7snc.xlsx',
+    'filename_old': 'files/7.xlsx',
+    'output_file': 'files/last_7.xlsx',
+    'week_val': '55',
+    'mongo_client': "mongodb+srv://drkbluescience:BlueWorld.001.@cluster0.nva1n.mongodb.net/Match?retryWrites=true&w=majority",
+}
+keys = {"Hafta": "101", "Gun": "Cuma", "Tarih": "01.01.2021", "Lig": " AFK", "EvSahibi": "A",
+                     "Misafir": "B", "IY": "2 - 1", "MS": "2 - 5", "MS1": "2,55", "MSX": "2,85", "MS2": "2",
+                     "IY1": "2,9", "IYX": "1,9", "IY2": "2,35", "KGVar": "1,5", "KGYok": "1,9", "birX": "1,35",
+                     "bir2": "1,25", "x2": "1,2", "iy15a": "1,35", "iy15u": "2,2", "au15a": "2,9", "au15u": "1,18",
+                     "au25a": "1,75", "au25u": "1,6", "au35a": "1,3", "au35u": "2,4", "sifir1": "2,9", "iki3": "1,67",
+                     "dort6": "2,8", "yediplus": "4,6"}
